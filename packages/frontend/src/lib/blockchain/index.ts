01a33a3d6f6464635bd4f123102fff68e4b4e5d6
jbelanger <joblo10@hotmail.com>
2024-12-21 11:42:39 -0500

    wip

--- packages/frontend/src/lib/blockchain/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './config'
-export * from './abis/RepairRequestContract'
-export * from './abis/WorkOrderContract'
-export * from './services/RepairRequestService'
-export * from './services/WorkOrderService'
